8bd18b328dc70d403523c548641c6c0a4784da0c
Tahir2022 <abbasovtahir617@gmail.com>
2026-03-09 21:54:30 +0100

    Some stuff

---
+++ mad_libs.py
@@ -0,0 +1,14 @@
+def get_input(word_type: str):
+    user_input: str = input(f"Enter a {word_type}: ")
+    return user_input
+
+noun1 = get_input("noun")
+adjective1 = get_input("adjective")
+verb = get_input("verb")
+noun2 = get_input("")
+verb2 = get_input("verb")
+
+story = """
+
+"""
+print(story)
