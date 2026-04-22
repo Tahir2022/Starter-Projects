bdaebcce345884272ecc4cfdb6a00dea925a6906
Tahir2022 <abbasovtahir617@gmail.com>
2026-04-22 20:41:23 +0200

    second commit

--- age.py
+++ age.py
@@ -1,4 +1,11 @@
-user_age = input('Enter your age: ')
-years = int(user_age)
-months = years * 12 
-print(f'Your age, {years}, is equal to {months} months')
+
+#user_age = input('Enter your age: ')
+#years = int(user_age)
+#months = years * 12 
+#print(f'Your age, {years}, is equal to {months} months')
+
+friends = {"Rolf", "Bob", "Anne"}
+abroad = {"Bob", "Anne"}
+
+local_friends = friends.difference(abroad)
+print(local_friends)
